6d1db3d3e9e553817f955bb555ccddad92c37a4e
debdatta-chatterjee <debdattachatterjee14@gmail.com>
2025-10-17 09:10:32 +0530

    added readme file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="shipment",
+    version='0.0.1',
+    author='debdatta',
+    author_email='debdattachatterjee14@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
